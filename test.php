0c498c81f3a35208a61b21a90ae925487fe8f04b
AndhA <a17dh4@yahoo.com>
2014-02-14 11:34:23 -0800

    Commit Unit Test for Task 1.1 and 1.2.

---
+++ test.php
@@ -0,0 +1,10 @@
+<?php
+
+include "fizz_buzz.php";
+include "fiz_buzz_bazz.php";
+
+echo "FizzBuzz(1,30) : <br>".FizzBuzz(1,30);
+echo "<br><br>";
+echo "FizzBuzzBazz(1,30) : <br>".FizzBuzzBazz(1,30);
+
+?>
